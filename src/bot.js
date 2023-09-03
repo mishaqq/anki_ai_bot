a95df8e20b9974e4bb1771e1598355bac50ce544
mishaqq <dron7432@gmail.com>
2023-09-03 08:13:50 +0200

    MIT License

--- src/bot.js
+++ src/bot.js
@@ -1,5 +1,6 @@
 import http from 'http';
-
+import dotenv from 'dotenv'
+dotenv.config()
 
 const hostname = '127.0.0.1';
 const port = 3000;
@@ -7,7 +8,7 @@ const port = 3000;
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello node');
+    res.end(process.env.TELEGRAM_KEY);
 });
 
 server.listen(port, hostname, () => {
